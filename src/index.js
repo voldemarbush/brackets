ab7d9e9b70f8f2cfd1bd721fc7c9eeaa8b587abb
Vladimir Bushev <voldemarbush@gmail.com>
2022-02-06 13:30:54 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const opens = [];
+  const closes = [];
+  const brObj = {};
+  const stack = [];
+  let current = '';
+
+  if (str.length % 2 !== 0) return false;
+
+  for (a = 0; a < bracketsConfig.length; a++) {
+    brObj[`${bracketsConfig[a][1]}`] = bracketsConfig[a][0];
+    opens[a] = bracketsConfig[a][0];
+    closes[a] = bracketsConfig[a][1];
+  }
+
+  for (a = 0; a < str.length; a++) {
+    current = str[a];
+    if (closes.includes(current)) {
+      if (brObj[current] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    } else {
+      stack.push(current);
+    }
+  }
+  return stack.length === 0;
 }
